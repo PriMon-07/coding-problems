86c1e22585ffd9817c12b708815371ef8c88151d
Pritam Mondal <pritam.mondal.pm@live.com>
2025-06-01 09:56:46 +0530

    feat: more problems

--- src/problems/sliding_window/LongestSubstringWithoutRepeatingCharacters.java
+++ src/problems/sliding_window/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,6 +1,8 @@
 package problems.sliding_window;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 /**
@@ -44,11 +46,40 @@ public class LongestSubstringWithoutRepeatingCharacters {
         int max = 0;
         for (int j = 0; j < s.length(); j++) {
             char c = s.charAt(j);
+            // while slides the left pointer till it finds a character that is not in the set
             while (set.contains(c)) {
                 set.remove(s.charAt(i));
                 i++;
             }
             set.add(c);
+            max = Math.max(max, j - i + 1);
+        }
+
+        return max;
+    }
+
+    /**
+     * Finds the length of the longest substring without repeating characters in a given string, using an optimal approach.
+     *
+     * @param s the input string
+     * @return the length of the longest substring without repeating characters
+     *
+     * Time Complexity: O(n), where n is the length of the string. This is because each character is visited at most twice.
+     * Space Complexity: O(min(n, m)), where n is the length of the string and m is the character set size.
+     */
+    public int lengthOfLongestSubstringOptimal(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+
+        int i = 0;
+        int max = 0;
+        for (int j = 0; j < s.length(); j++) {
+            char c = s.charAt(j);
+            if (map.containsKey(c)) {
+                // max method is required for edge case - "abba"
+                // max method is required if left pointer is moved away from the index we get from map
+                i = Math.max(i, map.get(c) + 1);
+            }
+            map.put(c, j);
             max = Math.max(max, j - i + 1);
         }
 
@@ -63,5 +94,10 @@ public class LongestSubstringWithoutRepeatingCharacters {
         System.out.println(obj.lengthOfLongestSubstring("bbbbb")); // 1
         System.out.println(obj.lengthOfLongestSubstring("pwwkew")); // 3
         System.out.println(obj.lengthOfLongestSubstring("")); // 0
+
+        System.out.println(obj.lengthOfLongestSubstringOptimal("abcabcbb")); // 3
+        System.out.println(obj.lengthOfLongestSubstringOptimal("bbbbb")); // 1
+        System.out.println(obj.lengthOfLongestSubstringOptimal("pwwkew")); // 3
+        System.out.println(obj.lengthOfLongestSubstringOptimal("")); // 0
     }
 }
